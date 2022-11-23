456f3e5d7b4a51bd054d44b75d09e0ab2eee7082
bepbepimasheep <113216082+bepbepimasheep@users.noreply.github.com>
2022-11-23 19:40:16 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+console.log("Meu primeiro projeto no console");
+
+var firstname ="carambolha";
+var lastname ="baiconi";
+
+var fullname=firstname.concat(lastname);
+console.log(fullname);
+
+var a = 10;
+var b= 5;
+
+var add = a +b;
+console.log(add);
+
+var sub = a -b;
+console.log(sub);
+
+var mult= a * b;
+console.log(mult);
+
+var div =a/b;
+console.log(div);
